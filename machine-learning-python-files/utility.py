984bfa1eb452031f78788b7928fb69dce7d151fa
zkunchen <zkuchen@ucdavis.edu>
2021-11-24 00:25:53 -0800

    Update utility.py

--- machine-learning-python-files/utility.py
+++ machine-learning-python-files/utility.py
@@ -1,10 +1,9 @@
 import numpy as np
+import os
 from sklearn.model_selection import cross_validate
 from sklearn.linear_model import LassoLars
 from sklearn.metrics import make_scorer, mean_absolute_error
 import subprocess
-import os
-
 
 # --------------------------------------------------------------------------------- #
 def cross_validation_scheme(X, Y, estimator=None, objective_function=None, n_cv=10):
@@ -24,7 +23,8 @@ def cross_validation_scheme(X, Y, estimator=None, objective_function=None, n_cv=
     if estimator is None:
         estimator = LassoLars(alpha=1.2e-6, max_iter=10000, normalize=True, fit_intercept=True)
     if objective_function is None:
-        # Config scoring function
+        
+	# Config scoring function
         mean_signed_error_score = make_scorer(mean_signed_error)
         mean_absolute_error_score = make_scorer(mean_absolute_error)
 
@@ -83,7 +83,7 @@ def transfer_learning_from_pre_train_models(estimator_para_file_path,
                                             descriptor,
                                             ground_truth,
                                             molecule_string):
-    """transfer_learning_from_pre_train_models is used to perform transfer-learning from pretrain model
+    """transfer_learning_from_pre_train_models is used to perform transfer-learning from pretrain models
 
             Input:
                 estimator_para_file_path: (str) estimator parameter files
@@ -130,7 +130,8 @@ def make_estimators(estimator_para_file_path, estimator_base_structure=None):
  
     # Initialize base structure for the estimators
     if estimator_base_structure is None:
-        # If no base structure is provided, generate a list of base structure with the same lasso models
+        
+	# If no base structure is provided, generate a list of base structures with the same lasso model configurations
         for j in range(len(estimator_para_files)):
             estimator_base_structures.append(LassoLars(alpha=1.2e-6, max_iter=10000, normalize=True, fit_intercept=True))
 
@@ -163,21 +164,23 @@ def mean_signed_error(y_true, y_pred):
 
 # --------------------------------------------------------------------------------- #
 def extract_dataset(data_path, is_extract_pre_train_model=False):
-    """extract_dataset is used to extract data set of descriptors and target energies
+    """extract_dataset is used to extract data set of descriptors, target energies and pretrain models
 
         Input:
             data_path: (str) name of the output file
+	    is_extract_pre_train_model (boolean): Instruction on whether to extract pretrain model tar gz files
     """
 
     # Declare descriptor and target energy paths accordingly
     descriptor_path = data_path + '/' + 'descriptors.tar.gz'
     target_energy_path = data_path + '/' + 'target_energies.tar.gz'
 
-    # extract data sets
+    # Extract descriptors and target energies
     print('Descriptor and target energy data extracting ...\n')
     subprocess.call('tar xzvf ' + descriptor_path, shell=True)
     subprocess.call('tar xzvf ' + target_energy_path, shell=True)
 
+    # Extract pre-train models	
     if is_extract_pre_train_model:
         print('Pretrain model extracting ...\n')
         pre_train_model_path = data_path + '/' + 'pre_train_models.tar.gz'
